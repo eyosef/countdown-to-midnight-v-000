2b23cc6fea18baaeedf00eb39b9f35ee516b253a
Eden Mesfun <emesfun@gmail.com>
2018-01-23 19:20:43 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,8 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    puts "HAPPY NEW YEAR!"
   end #while loop
+      puts "HAPPY NEW YEAR!"
 end #method
 
 def countdown_with_sleep
