ce2eedc714a2ef61446c7d40b3d88d9724f8b8a7
Eden Mesfun <emesfun@gmail.com>
2018-01-23 19:19:24 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 
 
 def countdown(number)
-while number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end 
 end
 
 def countdown_with_sleep
